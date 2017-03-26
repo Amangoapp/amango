895d5ce48f5269091e97dec5f7eaa873a948dbfc
Hum4n01d <hum4n01d@icloud.com>
2017-03-25 21:13:46 -0700

    Added tabs

--- frontend/renderer.js
+++ frontend/renderer.js
@@ -1,11 +1,38 @@
-document.getElementById("soundcloud").onclick = function() { soundcloud() };
-document.getElementById("scratch").onclick = function() { scratch() };
+"use strict";
 
+var tabs = document.querySelectorAll(".tab");
+var tabNavWrap = document.querySelector(".tabs-nav");
 
-function soundcloud() {
-    document.getElementById("output").innerHTML = "Soundcloud";
+function generateNav() {
+  for (var i = 0; i < tabs.length; i++) {
+    var navTab = document.createElement("button");
+    var index = i + 1;
+
+    navTab.innerText = "Tab " + index;
+    navTab.classList += "nav-tab-item"
+    navTab.setAttribute('data-id', index);
+
+    tabNavWrap.appendChild(navTab);
+  }
 }
 
-function scratch() {
-    document.getElementById("output").innerHTML = "Scratch";
+function update(activeTabIndex) {
+  for (var i = 0; i < tabs.length; i++) {
+    var tab = tabs[i];
+
+    if (i === activeTabIndex) {
+      tab.style.display = "flex";
+    } else {
+      tab.style.display = "none";
+    }
+  }
 }
+
+tabNavWrap.addEventListener('click', function (e) {
+  var newTabIndex = e.target.getAttribute('data-id') - 1;
+
+  update(newTabIndex);
+});
+
+generateNav();
+update(0);

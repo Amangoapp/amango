6f07b27c428cc6804b4a4f8ca25fe405c342523a
Hum4n01d <hum4n01d@icloud.com>
2017-03-26 10:30:09 -0700

    Added nav bar and basic (not working) code for back/forward

--- frontend/renderer.js
+++ frontend/renderer.js
@@ -3,21 +3,63 @@
 var tabs = document.querySelectorAll(".tab");
 var tabNavWrap = document.querySelector(".tabs-nav");
 var lastActiveTab = null
+var newTab = document.querySelector("button#new-tab");
+var tabsWrap = document.querySelector(".tabs");
+var backForward = document.querySelector('.back-forward')
+var partitionI = 0
+
+var numTabs = 0
+
+backForward.addEventListener('click', (e) => {
+  var el = e.target
+  var webviews = document.querySelectorAll('.tab webview')
+  var webviewIndex = lastActiveTab.getAttribute('data-id') - 1
+  var webview = webviews[webviewIndex]
+
+  webview.addEventListener("dom-ready", function(){
+    webview.openDevTools();
+  });
+
+  if (el.className === 'navigation-backward') {
+    // webview.executeJavascript('window.history.back()')
+  } else if (el.className === 'navigation-forward') {
+
+  }
+})
+
+function addTab(index, url) {
+  var navTab = document.createElement("button");
+
+  navTab.innerText = "Soundcloud "+ index
+  navTab.classList += "nav-tab-item"
+  navTab.setAttribute('data-id', index);
+
+  var newTab = document.createElement('div')
+  newTab.classList.add('tab')
+
+  var webview = document.createElement('webview')
+  webview.src = url
+  webview.setAttribute('partition', partitionI)
+  partitionI++
+
+  newTab.appendChild(webview)
+
+  tabsWrap.appendChild(newTab)
+
+  tabNavWrap.appendChild(navTab);
+}
 
 function generateNav() {
   for (var i = 0; i < tabs.length; i++) {
-    var navTab = document.createElement("button");
     var index = i + 1;
 
-    navTab.innerText = "Soundcloud "+ index
-    navTab.classList += "nav-tab-item"
-    navTab.setAttribute('data-id', index);
-
-    tabNavWrap.appendChild(navTab);
+    addTab(index)
   }
 }
 
 function update(activeTabIndex) {
+  tabs = document.querySelectorAll(".tab")
+
   for (var i = 0; i < tabs.length; i++) {
     var tab = tabs[i];
 
@@ -30,15 +72,24 @@ function update(activeTabIndex) {
 }
 
 tabNavWrap.addEventListener('click', function (e) {
-  var newTabIndex = e.target.getAttribute('data-id') - 1;
-  console.log(lastActiveTab);
+  var clicked = e.target
+
+  if(clicked.id === 'new-tab') {return false;}
+
+  var newTabIndex = clicked.getAttribute('data-id') - 1;
+  console.log(clicked);
 
   try{lastActiveTab.classList.remove('active-nav-tab')} catch(e){}
-  lastActiveTab = e.target
+  lastActiveTab = clicked
 
   lastActiveTab.classList.add("active-nav-tab")
   update(newTabIndex);
 });
+
+newTab.addEventListener('click', function (e) {
+  addTab(numTabs+1, 'https://soundcloud.com')
+  numTabs++;
+})
 
 generateNav();
 update(0);

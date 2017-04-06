ec06b87e1705c8bdcd5569c58031f2d40daaad42
Hum4n01d <hum4n01d@icloud.com>
2017-04-06 14:43:17 -0700

    Now saving sessions!!!

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ let mainWindow
 
 app.on('ready', function() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     show: false,
     titleBarStyle: 'hidden'
   })
